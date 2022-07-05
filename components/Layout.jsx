b9b89666ad374738156cf238e4f14bb001bfcdd8
saggemode <saggemode@gmail.com>
2022-07-05 09:40:35 +0100

    footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
 
 const Layout = ({ title, description, children }) => {
@@ -10,12 +11,30 @@ const Layout = ({ title, description, children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
-        <header>heaer</header>
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">Tochi Store</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    2
+                  </span>
+                </a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
 
-        <main>{children}</main>
+        <main className="container m-auto mt-4 px-4">{children}</main>
 
         <footer className="flex h-10 justify-center items-center shadow-inner">
-            
           <p>Copyright © 2022 Tochi Store</p>
         </footer>
       </div>
