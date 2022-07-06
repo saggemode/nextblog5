4f7323adf49211ed8551ced4467dca7a360de7e0
saggemode <saggemode@gmail.com>
2022-07-06 16:15:59 +0100

    all

--- pages/api/orders/history.jsx
+++ pages/api/orders/history.jsx
@@ -1,4 +1,3 @@
-// /api/orders/:id
 import { getSession } from 'next-auth/react';
 import db from '../../../utils/db';
 import Order from '../../models/Order';
@@ -7,14 +6,13 @@ import Order from '../../models/Order';
 const handler = async (req, res) => {
   const session = await getSession({ req });
   if (!session) {
-    return res.status(401).send('signin required');
+    return res.status(401).send({ message: 'signin required' });
   }
-
+  const { user } = session;
   await db.connect();
-
-  const order = await Order.findById(req.query.id);
+  const orders = await Order.find({ user: user._id });
   await db.disconnect();
-  res.send(order);
+  res.send(orders);
 };
 
 export default handler;
